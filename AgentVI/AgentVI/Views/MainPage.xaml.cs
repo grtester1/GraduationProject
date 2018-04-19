e7061ded8367cf9a6f2bf45acbca0b896225973d
Amikam Goldfarb <Amikam.Goldfarb@wdc.com>
2018-04-20 02:28:12 +0300

    App redesigned, initial code

---
+++ AgentVI/AgentVI/Views/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AgentVI.Views
+{
+	public partial class MainPage : ContentPage
+	{
+        void FooterBarEvents_Clicked(object sender, EventArgs e)
+        {
+            PlaceHolder.Content = (new Page1()).Content;
+        }
+        void FooterBarCameras_Clicked(object sender, EventArgs e)
+        {
+            PlaceHolder.Content = (new Page2()).Content;
+        }
+        void FooterBarHealth_Clicked(object sender, EventArgs e)
+        {
+            PlaceHolder.Content = (new Page2()).Content;
+        }
+        void FooterBarSettings_Clicked(object sender, EventArgs e)
+        {
+            PlaceHolder.Content = (new Page2()).Content;
+        }
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
