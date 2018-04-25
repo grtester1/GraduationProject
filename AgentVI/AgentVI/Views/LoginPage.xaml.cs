cd6c1cd736ba8128797239df2aeef20224ff682f
gil ronen <gilgilronen@gmail.com>
2018-04-26 01:49:05 +0300

    improves Login Page

--- AgentVI/AgentVI/Views/LoginPage.xaml.cs
+++ AgentVI/AgentVI/Views/LoginPage.xaml.cs
@@ -9,51 +9,64 @@ using InnoviApiProxy;
 
 namespace AgentVI.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class LoginPage : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
         LoginResult m_loginResult = null;
-        public LoginPage ()
-		{
-            InitializeComponent ();
+
+        public LoginPage()
+        {
+            InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
         }
 
         void loginButton_Clicked(object sender, EventArgs e)
-        {   
-            if(m_loginResult == null)
-            {
-                try
-                {
-                    m_loginResult = User.Login(usernameEntry.Text, passwordEntry.Text);
-                }
-                catch (Exception ex)
-                {
-                    DisplayAlert("Exception", ex.Message, "Close");
-                }
-            }
-            if(m_loginResult != null)
-            {
-                if (m_loginResult.ErrorMessage == LoginResult.eErrorMessage.Empty)
-                {
-                    ViewModels.TemporaryShit.getInstance().Username = m_loginResult.User.Username;
-                    ViewModels.TemporaryShit.getInstance().LoggedInUser = m_loginResult.User;
-                    ViewModels.TemporaryShit.getInstance().InitializeFields();
-                    Navigation.PushModalAsync(new MainPage());
-                }
-                else
-                {
-                    DisplayAlert("Error Message", m_loginResult.ErrorMessage.toString(), "Retry");
-                    m_loginResult = null;
-                }
-            }
+        {
+            string userName = usernameEntry.Text;
+            string password = passwordEntry.Text;
+            bool isUserNameEmpty = string.IsNullOrEmpty(userName);
+            bool isPasswordEmpty = string.IsNullOrEmpty(password);
 
-            
+            if (isUserNameEmpty || isPasswordEmpty)
+            {
+                DisplayAlert("Login Error", "Please enter your user name and your password.", "OK");
+            }
+            else
+            {
+                if (m_loginResult == null)
+                {
+                    try
+                    {
+                        m_loginResult = User.Login(userName, password);
+                    }
+                    catch (Exception ex)
+                    {
+                        DisplayAlert("Exception", ex.Message, "Close");
+                    }
+
+                }
+
+                if (m_loginResult != null)
+                {
+                    if (m_loginResult.ErrorMessage == LoginResult.eErrorMessage.Empty)
+                    {
+                        ViewModels.TemporaryShit.getInstance().Username = m_loginResult.User.Username;
+                        ViewModels.TemporaryShit.getInstance().LoggedInUser = m_loginResult.User;
+                        ViewModels.TemporaryShit.getInstance().InitializeFields();
+                        Navigation.PushModalAsync(new MainPage());
+                    }
+                    else
+                    {
+                        DisplayAlert("Login Error", m_loginResult.ErrorMessage.toString(), "OK");
+                        m_loginResult = null;
+                    }
+                }
+            }
         }
 
         async void forgotPwdButton_Clicked(object sender, EventArgs e)
         {
-            var response = await DisplayActionSheet("Title", "Cancel", "Delete", "Copy Link", "Duplicate Link");
+            var response = await DisplayActionSheet("Forgot your password?", "Cancel", "Delete", "Copy Link", "Duplicate Link");
             await DisplayAlert("Response", response, "OK");
         }
 
