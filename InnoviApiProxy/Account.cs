da3994e584a73cfd5fe7a506c563403d66630872
nadav ramot <ramot.n@gmail.com>
2018-05-06 13:43:03 +0300

    proxy update
    added option to switch accounts

--- InnoviApiProxy/Account.cs
+++ InnoviApiProxy/Account.cs
@@ -8,7 +8,7 @@ namespace InnoviApiProxy
     public class Account : InnoviObject
     {
         [JsonProperty("id")]
-        private string accountId { get; set; }
+        private int accountId { get; set; }
         [JsonProperty]
         public string Name { get; private set; }
         [JsonProperty]
@@ -20,6 +20,12 @@ namespace InnoviApiProxy
             Undefined,
             Suspended
         }
+
+        public void SetAsDefaultAccount()
+        {
+            HttpUtils.SwitchAccount(accountId);
+        }
+
 
         internal Account() { }
     }
