976ff3dee29e3e32392ed9dba87a4395a750c55d
gil ronen <gilgilronen@gmail.com>
2018-05-09 22:28:38 +0300

    saving settings and credentials - DONE
    DONE
    only the logut button at settings page is not finished.

--- AgentVI/AgentVI/Views/LoadingPage.xaml.cs
+++ AgentVI/AgentVI/Views/LoadingPage.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using AgentVI.Services;
 using Xamarin.Forms;
-
+using InnoviApiProxy;
+using AgentVI.ViewModels;
 namespace AgentVI.Views
 {
     public partial class LoadingPage : ContentPage
@@ -17,9 +18,37 @@ namespace AgentVI.Views
         {
             base.OnAppearing();
 
-            await ProgressBarLine.ProgressTo(1, 3000, Easing.CubicInOut);
+            await ProgressBarLine.ProgressTo(0.1, 1000, Easing.SinIn);
 
-            await Navigation.PushAsync(new LoginPage());
+            if (ServiceManager.Instance.LoginService.DoCredentialsExist()) //if the user is saved
+            {
+                LoginResult loginResult = User.Connect(ServiceManager.Instance.LoginService.AccessToken);
+                //LoginResult loginResult = User.Login("gilgilronen@gmail.com", "password");
+
+                await ProgressBarLine.ProgressTo(0.7, 2000, Easing.CubicIn);
+
+                if (loginResult.ErrorMessage == LoginResult.eErrorMessage.Empty) //if the access token is valid and not expired
+                {
+                    ServiceManager.Instance.LoginService.setLoggedInUser(loginResult.User);
+                    await ProgressBarLine.ProgressTo(1, 2000, Easing.Linear);
+                    Navigation.InsertPageBefore(new MainPage(), this);
+                    await Navigation.PopAsync();
+                }
+                else
+                {
+                    await ProgressBarLine.ProgressTo(1, 1000, Easing.Linear);
+                    Navigation.InsertPageBefore(new LoginPage(), this);
+                    await Navigation.PopAsync();
+                    //await Navigation.PushAsync(new LoginPage());
+                }
+            }
+            else
+            {
+                await ProgressBarLine.ProgressTo(1, 1000, Easing.Linear);
+                Navigation.InsertPageBefore(new LoginPage(), this);
+                await Navigation.PopAsync();
+                //await Navigation.PushAsync(new LoginPage());
+            }
         }
     }
 }
