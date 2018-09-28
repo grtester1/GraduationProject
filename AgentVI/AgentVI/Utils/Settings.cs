92459824783ed495e7cbcb01c864515be9845b83
7308521 <amikam.goldfarb@wdc.com>
2018-09-29 02:13:06 +0300

    Settings class and xaml code improvement

--- AgentVI/AgentVI/Utils/Settings.cs
+++ AgentVI/AgentVI/Utils/Settings.cs
@@ -9,8 +9,16 @@ namespace AgentVI.Utils
     {
         public static Thickness iOSPlatformPadding { get; } = new Thickness(5);
         public static Thickness ListViewMargins { get; } = new Thickness(0,5,0,5);
+
+        public static Color FooterColor { get; } = Color.AliceBlue;
+        public static Color FooterSeparatorColor { get; } = Color.FromHex("#DCDCDC");
+        public static Color SensorEventFontColor { get; } = Color.Navy;
+
         public static string LoadingAnimationPath { get; } = "resource://AgentVI.Sources.LoadingAnimation.gif";
         public static string BackgroundPicturePath { get; } = "resource://AgentVI.Sources.background.png";
         public static string LogoPicturePath { get; } = "resource://AgentVI.Sources.innovi_logo.png";
+
+        public static double SensorEventNameFontSize { get; } = 20;
+        public static double SensorEventDetailsFontSize { get; } = 16;
     }
 }
