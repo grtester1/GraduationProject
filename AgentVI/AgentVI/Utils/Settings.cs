d35356754fef1eef72b5e4c36d55fd704152cf1f
7308521 <amikam.goldfarb@wdc.com>
2018-09-29 01:31:46 +0300

    sources fix

--- AgentVI/AgentVI/Utils/Settings.cs
+++ AgentVI/AgentVI/Utils/Settings.cs
@@ -9,6 +9,8 @@ namespace AgentVI.Utils
     {
         public static Thickness iOSPlatformPadding { get; } = new Thickness(5);
         public static Thickness ListViewMargins { get; } = new Thickness(0,5,0,5);
-        public static String LoadingAnimationPath { get; } = "resource://AgentVI.Sources.LoadingAnimation.gif";
+        public static string LoadingAnimationPath { get; } = "resource://AgentVI.Sources.LoadingAnimation.gif";
+        public static string BackgroundPicturePath { get; } = "resource://AgentVI.Sources.background.png";
+        public static string LogoPicturePath { get; } = "resource://AgentVI.Sources.innovi_logo.png";
     }
 }
