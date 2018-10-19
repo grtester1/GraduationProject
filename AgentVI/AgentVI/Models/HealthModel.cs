735460d77aa976018de197cf50dec83429aba0d4
Gil Ronen <gilgilronen@gmail.com>
2018-10-19 18:28:31 +0300

    health by filtering
    DONE!

--- AgentVI/AgentVI/Models/HealthModel.cs
+++ AgentVI/AgentVI/Models/HealthModel.cs
@@ -1,11 +1,40 @@
 ﻿using System;
+using System.ComponentModel;
+using InnoviApiProxy;
+using Xamarin.Forms;
 
 namespace AgentVI.Models
 {
     public class HealthModel
     {
+        private HealthModel()
+        {
+
+        }
+
+        internal static HealthModel FactoryMethod(Sensor.Health i_Health)
+        {
+            HealthModel res = new HealthModel()
+            {
+                HealthTime = (ulong)i_Health.StatusTimeStamp,
+                HealthDescription = i_Health.DetailedDescription,
+                HealthDuration = "00:00" // nadav needs to implement duration calculation function 
+            };
+
+            return res;
+        }
+
+        public ulong HealthTime { get; set; }
+        public string HealthDescription { get; set; }
+        public string HealthDuration { get; set; }
+
+
+        //The old HealthModel.cs:
+        //---------------------------
+        /*
         public long HealthTime { get; set; }
         public string HealthDescription { get; set; }
         public string HealthDuration { get; set; }
+        */
     }
 }
--- AgentVI/AgentVI/Views/HealthPage.xaml.cs
+++ AgentVI/AgentVI/Views/HealthPage.xaml.cs
@@ -8,13 +8,75 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 using AgentVI.ViewModels;
 using AgentVI.Models;
+using AgentVI.Services;
+using AgentVI.Interfaces;
+using AgentVI.Utils;
+using System.Threading.Tasks;
+using System.Collections;
+
 
 namespace AgentVI.Views
 {
-    public partial class HealthPage : ContentPage
+    public partial class HealthPage : ContentPage//, INotifyContentViewChanged
     {
         private HealthListViewModel HealthPageVM = null;
+        //public event EventHandler<UpdatedContentEventArgs> RaiseContentViewUpdateEvent;
 
+
+
+        public HealthPage()
+        {
+            InitializeComponent();
+            HealthPageVM = new HealthListViewModel();
+            initOnFilterStateUpdatedEventHandler();
+            HealthPageVM.PopulateCollection();
+            healthListView.BindingContext = HealthPageVM;
+        }
+
+        /*
+        public HealthPage(SensorModel i_sensor) : this()
+        {
+            HealthPageVM = new HealthListViewModel(i_sensor);
+            cameraNameHeader.Text = "Health for " + i_sensor.SensorName;
+            Title = i_sensor.SensorName;
+            listView.ItemsSource = HealthPageVM.HealthsList;
+        }*/
+
+
+
+
+
+
+
+        private void initOnFilterStateUpdatedEventHandler()
+        {
+            ServiceManager.Instance.FilterService.FilterStateUpdated += HealthPageVM.OnFilterStateUpdated;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            HealthPageVM.PopulateCollection();
+        }
+
+        private async void OnRefresh(object sender, EventArgs e)
+        {
+            try
+            {
+                await Task.Factory.StartNew(() => HealthPageVM.PopulateCollection());
+                ((ListView)sender).IsRefreshing = false;
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+  
+
+        //The old HealthPage.xaml.cs:
+        //---------------------------
+        /*
         public HealthPage()
         {
             InitializeComponent();
@@ -33,5 +95,6 @@ namespace AgentVI.Views
             await System.Threading.Tasks.Task.Factory.StartNew(() => HealthPageVM.UpdateHealthList());
             ((ListView)sender).IsRefreshing = false; //end the refresh state
         }
+        */
     }
 }
